3d8b82263e7df317f407addd6db61fa08fb3fad2
JulianSobott <julian.sobott@gmail.com>
2019-02-16 13:00:51 +0100

    Add NewConnectionListener

--- networking/Communication_server.py
+++ networking/Communication_server.py
@@ -7,3 +7,57 @@
 
 @internal_use:
 """
+import threading
+import socket
+
+from networking.Logging import logger
+from networking.Communication_general import Communicator
+
+
+class NewConnectionListener(threading.Thread):
+
+    def __init__(self, address):
+        super().__init__(name="NewConnectionListener")
+        self._socket_connection = socket.socket()
+        self._clients = []
+        self._next_client_id = 0
+        self._is_on = True
+        self._address = address
+        self._exit = threading.Event()
+
+    def run(self):
+        try:
+            self._socket_connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self._socket_connection.bind(self._address)
+            logger.debug(f"Server is now listening on: {self._address[0]}:{self._address[1]}")
+        except OSError:
+            # [WinError 10038] socket closed before
+            self._is_on = False
+
+        while self._is_on:
+            self._socket_connection.listen(4)
+            try:
+                (connection, addr) = self._socket_connection.accept()
+                logger.info("New client connected: (%s)", str(addr))
+                client = Communicator(self._address, self._produce_next_client_id(), connection)
+                client.start()
+                self._clients.append(client)
+            except OSError:
+                logger.error("TCP connection closed while listening")
+                # TODO: handle (if possible)
+
+    def _produce_next_client_id(self):
+        try:
+            return self._next_client_id
+        finally:
+            self._next_client_id += 1
+
+    def stop_listening(self):
+        self._is_on = False
+        self._socket_connection.close()
+        self.join()
+        logger.info("Closed server listener")
+
+    def stop_connections(self):
+        for t in self._clients:
+            t.join()
--- networking/tests/test_Communication.py
+++ networking/tests/test_Communication.py
@@ -1,6 +1,9 @@
 import unittest
 
 from networking.Communication_Client import *
+from networking.Communication_server import *
+
+dummy_address = ("127.0.0.1", 5000)
 
 
 class TestCommunicator(unittest.TestCase):
@@ -10,15 +13,23 @@ class TestCommunicator(unittest.TestCase):
 class TestClientCommunicator(unittest.TestCase):
 
     def test_connection(self):
-        addr = ("127.0.0.1", 5000)
 
-        DummyServerCommunicator.connect(addr)
+        DummyServerCommunicator.connect(dummy_address)
         self.assertIsInstance(ServerCommunicator.communicator, Communicator)
         self.assertIsInstance(DummyServerCommunicator.communicator, Communicator)
 
         DummyServerCommunicator.close_connection()
         self.assertIsInstance(ServerCommunicator.communicator, type(None))
         self.assertIsInstance(DummyServerCommunicator.communicator, type(None))
+
+
+class TestNewConnectionListener(unittest.TestCase):
+
+    def test_open_close(self):
+        listener = NewConnectionListener(dummy_address)
+        listener.start()
+        listener.stop_listening()
+        self.assertEqual(listener._is_on, False)
 
 
 if __name__ == '__main__':
