89337b5f81c8ff0232c9070956262af0e0348254
JulianSobott <julian.sobott@gmail.com>
2019-02-19 10:45:09 +0100

    Remove old tests

--- networking/tests/test_Communication.py
+++ networking/tests/test_Communication.py
@@ -115,68 +115,6 @@ class TestConnecting(unittest.TestCase):
             self.assertEqual(len(listener.clients.values()), 0)
 
 
-class TestCommunicator(unittest.TestCase):
-    pass
-
-
-class TestClientCommunicator(unittest.TestCase):
-
-    def test_connection_single(self):
-
-        DummyServerCommunicator.connect(dummy_address)
-        self.assertIsInstance(ServerCommunicator.communicator, Communicator)
-        self.assertIsInstance(DummyServerCommunicator.communicator, Communicator)
-
-        DummyServerCommunicator.close_connection()
-        self.assertIsInstance(ServerCommunicator.communicator, type(None))
-        self.assertIsInstance(DummyServerCommunicator.communicator, type(None))
-
-    def test_functions_communicator(self):
-        DummyMultiServerCommunicator(0).connect(dummy_address)
-        DummyMultiServerCommunicator(0).functions.dummy_function()
-
-
-class TestNewConnectionListener(unittest.TestCase):
-
-    def test_open_close(self):
-        listener = ClientManager(dummy_address)
-        listener.start()
-        listener.stop_listening()
-        self.assertEqual(listener._is_on, False)
-
-    def test_context_guard(self):
-        with ClientManager(dummy_address) as listener:
-            self.assertEqual(listener._is_on, True)
-            DummyServerCommunicator.connect(dummy_address)
-            DummyServerCommunicator.close_connection()
-        self.assertEqual(listener._is_on, False)
-        self.assertEqual(threading.active_count(), 1)
-
-    def test_single_connection(self):
-        with ClientManager(dummy_address):
-            DummyMultiServerCommunicator(0).connect(dummy_address)
-
-            time.sleep(1)   # provides a clearer output, but also works without
-            self.assertEqual(threading.active_count(), 4)
-
-            DummyMultiServerCommunicator(0).close_connection()
-
-        self.assertEqual(threading.active_count(), 1)
-
-    def test_multiple_connections(self):
-        with ClientManager(dummy_address):
-            DummyMultiServerCommunicator(0).connect(dummy_address)
-            DummyMultiServerCommunicator(1).connect(dummy_address)
-
-            time.sleep(1)  # provides a clearer output, but also works without
-            self.assertEqual(threading.active_count(), 6)
-
-            DummyMultiServerCommunicator(0).close_connection()
-            DummyMultiServerCommunicator(1).close_connection()
-
-        self.assertEqual(threading.active_count(), 1)
-
-
 class DummyServerCommunicator(ServerCommunicator):
     class _DummyServerFunctions(ServerFunctions):
         from networking_example.example_dummy_functions import dummy_no_arg_no_ret
