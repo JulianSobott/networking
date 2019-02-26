c072d81431736f656640690e3b4bc57aa6c327a2
JulianSobott <julian.sobott@gmail.com>
2019-02-26 11:20:28 +0100

    Update setup.py

--- README.rst
+++ README.rst
@@ -1,1 +1,5 @@
 High level network communication
+
+This tool abstracts network communication to a level, where the end user don`t has to care about
+network communication. Server side functions can be called at the client as they were local. Functions may be called
+with parameters and may return values.
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(name='networking',
-      version='0.1',
+      version='0.1a1',
       description='High level network communication',
+      long_description=long_description,
       url='https://github.com/JulianSobott/networking',
       author='Julian Sobott',
       author_email='julian.sobott@gmx.de',
-      license='Apache',
       packages=find_packages(),
       test_suite='nose.collector',
       tests_require=['nose'],
@@ -17,5 +20,12 @@ setup(name='networking',
         "Documentation": "https://github.com/JulianSobott/networking/wiki",
         "Source Code": "https://github.com/JulianSobott/networking",
       },
+      classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Monitoring",
+        ],
       zip_safe=False
       )
