98b0dfa7d739e909e416842371f3d409f5842ab7
JulianSobott <julian.sobott@gmail.com>
2019-04-12 18:23:51 +0200

    Add example with all features of pynetworking

--- networking_examples/single_file/all_functionality.py
+++ networking_examples/single_file/all_functionality.py
@@ -6,6 +6,15 @@ This is example is meant to demonstrate the use of the most important features o
 This example has to be adjusted if you want to implement a real client server application. Here client and
 server are executed in the same process which is ok for testing but not realistic in reality.
 
+Features:
+- call functions with arguments (server->client and client->server)
+- get return values
+- get Exceptions
+
+What cannot be returned by a function:
+- generator
+- frame
+- traceback
 """
 import pynetworking as net
 
@@ -18,6 +27,7 @@ net.Logging.logger.setLevel(40)
 
 def main():
     # open and start the server and close afterwards
+    print("This program shows all important features that are possible with the pynetworking library.\n\n")
     with net.ClientManager(server_address, ClientCommunicator):
         # Connect a client to the server
         ServerCommunicator.connect(server_address, blocking=True, timeout=2)
@@ -25,14 +35,20 @@ def main():
         # create an alias for all server functions (Not necessary, but code looks better)
         server = ServerCommunicator.remote_functions
 
-        # Call functions at the server and print the return values
-        ret = server.greet_client()
-        print(ret)
+        # Call functions at the server
+        print(f"{'='*7} Calling a function with arguments and return value {'=' * 7}\n")
+        types = server.get_types(1, "He", [1, 2], main, lambda a, b: a+b)
+        print(types)
 
-        ret = server.say_hello("Doris")
-        print(ret)
+        print(f"\n\n{'=' * 7} Calling a function that raises an error {'=' * 7}\n")
+        try:
+            server.risky_function()
+        except NotImplementedError as e:
+            print(e)
+            print("Error successfully transmitted")
 
-        server.set_username("Jack")
+        print(f"\n\n{'=' * 7} Calling a function that calls a function at the client {'=' * 7}\n")
+        server.start_communication()
 
         # Close the connection to the server at client-side
         ServerCommunicator.close_connection()
@@ -41,28 +57,36 @@ def main():
 class ClientFunctions(net.ClientFunctions):
     # A class that defines every method that can be called at a client from the server
     @staticmethod
-    def greet_server():
-        return ServerCommunicator.remote_functions.say_hello("Walter")
-
-    @staticmethod
-    def say_hello(name: str) -> str:
-        return f"Client: Hello {name}"
+    def a_client_function(message: str) -> None:
+        print(f"At Client: {message}")
+        ServerCommunicator.remote_functions.a_communication_function("Hi from Client!")
 
 
 class ServerFunctions(net.ServerFunctions):
     # A class that defines every method that can be called at the server from a client
     @staticmethod
-    def greet_client():
-        return net.ClientManager().get().remote_functions.say_hello("Walter")
+    def get_types(*args, **kwargs) -> list:
+        """returns a list of all types of the passed arguments.
+        e.g. simple_function(10, [1,2]) will return [int, list]"""
+        arg_types = []
+        for arg in args:
+            arg_types.append(type(arg))
+        for values in kwargs.values():
+            arg_types.append(type(values))
+        return arg_types
 
     @staticmethod
-    def say_hello(name: str) -> str:
-        return f"Server: Hello {name}"
+    def risky_function():
+        raise NotImplementedError("You successfully raised an error!")
 
     @staticmethod
-    def set_username(name: str):
-        net.ClientManager().get().username = name
-        print(f"Successfully set username to '{net.ClientManager().get().username}'")
+    def start_communication():
+        client: ClientCommunicator = net.ClientManager().get()
+        client.remote_functions.a_client_function("Hi from Server!")
+
+    @staticmethod
+    def a_communication_function(message):
+        print(f"At Server: {message}")
 
 
 class ClientCommunicator(net.ClientCommunicator):
